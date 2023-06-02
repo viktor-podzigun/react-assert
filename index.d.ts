f61ec10ba0006effe37ef3b3ee4c606aafa676b5
Viktor Podzigun <viktor.podzigun@gmail.com>
2023-06-02 09:01:26 +0200

    Added types for exported functions

--- index.d.ts
+++ index.d.ts
@@ -1,1 +1,12 @@
-export * from "./src/assertComponent";
+import React from "react";
+import TestRenderer from "react-test-renderer";
+
+export function assertComponent(
+  result: TestRenderer.ReactTestInstance | string,
+  expectedElement: React.ReactElement | string
+): void;
+
+export function assertComponents(
+  results: (TestRenderer.ReactTestInstance | string)[],
+  ...expectedElements: (React.ReactElement | string)[]
+): void;
